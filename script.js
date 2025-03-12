91cac760e495be050de1fb6e4e430ea0f7b9b936
Adit Zandika <101894352+Skukar@users.noreply.github.com>
2025-03-12 17:52:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let currentUserMessage = null;
 let isGeneratingResponse = false;
 
 const GOOGLE_API_KEY = "AIzaSyAg8P6rYIA63gJH82eyaCezUaU-Mx7tVpg";
-const API_REQUEST_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${GOOGLE_API_KEY}`;
+const API_REQUEST_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GOOGLE_API_KEY}`;
 
 // Load saved data from local storage
 const loadSavedChatHistory = () => {
